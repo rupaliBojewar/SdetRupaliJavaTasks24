5c53773a98db8f7a4256fd8fe7ab2bf745c676f8
Glogic <rupali.bojewar@glogic.co.in>
2024-04-14 15:55:23 +0530

    verify string are anagram or not

--- src/test/java/com/assignments/VerifyAngramStrings.java
+++ src/test/java/com/assignments/VerifyAngramStrings.java
@@ -1,16 +1,27 @@
 package com.assignments;
 
-import com.basicJava.ArrayPractice.Arrays;
+import java.util.Arrays;
 
 public class VerifyAngramStrings {
     public static void main(String[] args) {
-        verifyAnagramStrings("rupali","paliru");
+        verifyAnagramStrings("rupali","palirt");
     }
     static public void verifyAnagramStrings(String str1,String str2){
+        int cnt=0;
 
         char[] ch1 = str1.toCharArray();
         char[] ch2 = str2.toCharArray();
 
+        Arrays.sort(ch1);
         Arrays.sort(ch2);
+
+        for (int i = 0; i < ch1.length; i++) {
+            if (ch1[i]!=ch2[i])
+                cnt++;
+        }
+        if (cnt>0)
+            System.out.println("Strings are not Anagram");
+        else
+            System.out.println("Strings are Anagram");
     }
 }
